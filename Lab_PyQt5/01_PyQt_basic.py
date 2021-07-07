145f2959fb805ce87b952c49c40b758d1204d748
zibb03 <64014435+zibb03@users.noreply.github.com>
2021-07-07 20:38:48 +0900

    PyQt5 main function

---
+++ Lab_PyQt5/01_PyQt_basic.py
@@ -0,0 +1,20 @@
+# 01. pyqt_basic.py
+# PyQt5 기본 요소
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # 창 크기 고정
+        self.setFixedSize(1024, 768)
+        # 창 제목 설정
+        self.setWindowTitle('GA Mario')
+        # 창 띄우기
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
